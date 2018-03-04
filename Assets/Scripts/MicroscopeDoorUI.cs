814c83178258a324f3e5de7df9a717a0188f916e
tgrosh <tgrosh@yahoo.com>
2018-03-04 12:31:54 -0500

    Player can open and close Microscope Door

---
+++ Assets/Scripts/MicroscopeDoorUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicroscopeDoorUI : MonoBehaviour {
+    public Animator doorAnimator;
+    
+    public void Open()
+    {
+        doorAnimator.SetBool("isOpen", true);
+    }
+
+    public void Close()
+    {
+        doorAnimator.SetBool("isOpen", false);
+    }
+}
